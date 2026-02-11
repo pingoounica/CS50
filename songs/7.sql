1e5d3a5daa51a56b1264b894dff275aedffe6c9c
Vanessa Pires de Almeida <98252042+pingoounica@users.noreply.github.com>
2026-02-11 19:21:29 -0300

    Add files via upload

---
+++ songs/7.sql
@@ -0,0 +1,4 @@
+--In 7.sql, write a SQL query that returns the average energy of songs that are by Drake.
+--Your query should output a table with a single column and a single row containing the average energy.
+--You should not make any assumptions about what Drake’s artist_id is.
+select avg(energy) from songs where artist_id= (select id from artists where name ='Drake');
